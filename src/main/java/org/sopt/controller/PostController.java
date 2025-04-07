b126dd567fc05745cc74c61fec4788dd08827970
kaswhy <sxshx818@naver.com>
2025-04-07 20:14:04 +0900

    feat(controller): 요청이 들어오면 게시물 제목 중복 조회 검사 및 예외 처리 분기

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -3,11 +3,18 @@ package org.sopt.controller;
 import java.util.List;
 import org.sopt.domain.Post;
 import org.sopt.service.PostService;
+import org.sopt.validator.PostValidator;
 
 public class PostController {
     private final PostService postService = new PostService();
+    private final PostValidator postValidator = new PostValidator(postService);
 
     public String createPost(final String title) {
+        try {
+            postValidator.isDuplicatedTitle(title);
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
         return postService.createPost(title);
     }
 
