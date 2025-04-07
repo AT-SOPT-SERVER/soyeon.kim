30b55b21bd3ebd4a6d9fad2960f6f0f5c7447823
kaswhy <sxshx818@naver.com>
2025-04-07 17:16:06 +0900

    feat(domain): 도메인에 이름을 비워둘 수 없다는 규칙 추가

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -5,6 +5,9 @@ public class Post {
     private String title;
 
     public Post(int id, String title) {
+        if(isTitleBlank(title)) {
+            throw new IllegalArgumentException("⚠️ 게시글 제목은 비워둘 수 없습니다!");
+        }
         this.id = id;
         this.title = title;
     }
@@ -19,5 +22,9 @@ public class Post {
 
     public void updateTitle(String newTitle) {
         this.title = newTitle;
+    }
+
+    private boolean isTitleBlank(String title) {
+        return title.isBlank();
     }
 }
