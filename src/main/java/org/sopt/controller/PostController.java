7b7d936932615221e344e82ee6baa7b1c463c6c4
kaswhy <sxshx818@naver.com>
2025-04-07 15:40:07 +0900

    feat(sopt): 1차 과제 형상 맞추기

---
+++ src/main/java/org/sopt/controller/PostController.java
@@ -0,0 +1,25 @@
+package org.sopt.controller;
+
+import java.util.List;
+import org.sopt.domain.Post;
+import org.sopt.service.PostService;
+
+public class PostController {
+    private final PostService postService = new PostService();
+
+    public void createPost(final String title) {
+        postService.createPost(title);
+    }
+
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    public Post getPostById(int id) {
+        return postService.getPostById(id);
+    }
+
+    public boolean deletePostById(int id) {
+        return postService.deletePostById(id);
+    }
+}
