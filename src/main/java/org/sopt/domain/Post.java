f16e8758d7f8d8b68a6d73dc6c97c67272fa1e62
kaswhy <sxshx818@naver.com>
2025-04-09 20:52:00 +0900

    feat(domain): Post에 생성시각을 담을 createdAt 추가

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -1,13 +1,17 @@
 package org.sopt.domain;
 
+import java.time.LocalDateTime;
+
 public class Post {
     private final int id;
     private String title;
+    private final LocalDateTime createdAt;
 
     public Post(final int id, final String title) {
         validateTitle(title);
         this.id = id;
         this.title = title;
+        this.createdAt = LocalDateTime.now();
     }
 
     public int getId() {
@@ -18,22 +22,26 @@ public class Post {
         return this.title;
     }
 
-    public void updateTitle(final String newTitle) {
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void updateTitle(String newTitle) {
         this.title = newTitle;
     }
 
-    private void validateTitle(final String title) {
+    private void validateTitle(String title) {
         isTitleBlank(title);
         isTitleLessThan30(title);
     }
 
-    private void isTitleBlank(final String title) {
+    private void isTitleBlank(String title) {
         if (title.isBlank()) {
             throw new IllegalArgumentException("⚠️ 게시글 제목은 비워둘 수 없습니다!");
         }
     }
 
-    private void isTitleLessThan30(final String title) {
+    private void isTitleLessThan30(String title) {
         if (title.length() > 30) {
             throw new IllegalArgumentException("⚠️ 게시글 제목은 30자가 넘을 수 없습니다!");
         }
