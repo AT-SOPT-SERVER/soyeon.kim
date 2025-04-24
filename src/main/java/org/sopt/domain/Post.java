dfe03bcec79f660905e2f2cc0836c49c95f818cd
kaswhy <sxshx818@naver.com>
2025-04-25 00:00:54 +0900

    feat(domain): 기존 Post 도메인 내부 검증 로직 다시 추가

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -4,20 +4,47 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import org.sopt.common.exception.BusinessException;
+import org.sopt.common.util.GraphemeClusterUtil;
+import org.sopt.exception.PostErrorCode;
 
 @Entity
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
+
+    private LocalDateTime createdAt;
 
     public Post() {
 
     }
 
     public Post(String title) {
+        validateTitle(title);
         this.title = title;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    private void validateTitle(String title) {
+        isTitleBlank(title);
+        isTitleLessThan30(title);
+    }
+
+    private void isTitleBlank(String title) {
+        if (title.isBlank()) {
+            throw new BusinessException(PostErrorCode.INVALID_BLANK_TITLE);
+        }
+    }
+
+    private void isTitleLessThan30(String title) {
+        if (GraphemeClusterUtil.countGraphemeClusters(title) > 30) {
+            throw new BusinessException(PostErrorCode.INVALID_TITLE_LENGTH);
+        }
     }
 
     public Long getId() {
@@ -26,5 +53,9 @@ public class Post {
 
     public String getTitle() {
         return this.title;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
     }
 }
