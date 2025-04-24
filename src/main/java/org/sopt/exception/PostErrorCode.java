fc135c237895519beccf665fc509bb9b3af93a16
kaswhy <sxshx818@naver.com>
2025-04-25 01:31:38 +0900

    feat(exception): 해당 게시물 생성 안 됐는데 조회하는 경우에 대한 오류 코드 추가

--- src/main/java/org/sopt/exception/PostErrorCode.java
+++ src/main/java/org/sopt/exception/PostErrorCode.java
@@ -10,6 +10,9 @@ public enum PostErrorCode implements ErrorCode {
     INVALID_TITLE_LENGTH(HttpStatus.BAD_REQUEST, "⚠️ 게시글 제목은 30자를 넘을 수 없습니다!"),
     INVALID_CREATE_TIME(HttpStatus.BAD_REQUEST, "⚠️ 3분이 지나야 새 글을 작성할 수 있습니다."),
 
+    // 404 NOT FOUND
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "⚠️ 해당 게시글을 찾을 수 없습니다!"),
+
     // 409 CONFLICT
     TITLE_DUPLICATED(HttpStatus.CONFLICT, "⚠️ 게시물 제목은 중복될 수 없습니다!");
 
