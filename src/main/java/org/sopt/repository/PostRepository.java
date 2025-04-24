29078f50a40e722aa960933270c9541a471eeb77
kaswhy <sxshx818@naver.com>
2025-04-25 00:03:22 +0900

    feat(repository): 게시물 생성 시 검증에 사용하는 jpa 함수 추가

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -1,11 +1,18 @@
 package org.sopt.repository;
 
+import java.util.Optional;
 import org.sopt.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post findPostById(Long id);
-    void deleteById(Long id);
+    Optional<Post> findFirstByOrderByCreatedAtDesc();
+
+    Optional<Post> findPostById(Long id);
+
+    Optional<Post> findPostByTitle(String title);
+
+    void deleteById(@NonNull Long id);
 }
