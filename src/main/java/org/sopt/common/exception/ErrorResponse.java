877eccefc9aa6fb947e35056a7b9b03b6ce55e50
kaswhy <sxshx818@naver.com>
2025-04-24 19:32:08 +0900

    feat(exception): 예외 공통 응답 클래스 작성

---
+++ src/main/java/org/sopt/common/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package org.sopt.common.exception;
+
+public class ErrorResponse {
+
+    private final int status;
+    private final String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus().value();
+        this.message = errorCode.getMessage();
+    }
+}
