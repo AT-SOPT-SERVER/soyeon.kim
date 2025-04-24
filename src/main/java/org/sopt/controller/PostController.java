36dd2b4165550e756d10729918f866d55249f373
kaswhy <sxshx818@naver.com>
2025-04-25 00:01:39 +0900

    feat(controller): 게시물 생성 컨트롤러 구현 및 다른 api 매핑 추가

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -1,5 +1,7 @@
 package org.sopt.controller;
 
+import java.net.URI;
+import org.sopt.common.response.ApiResponse;
 import org.sopt.domain.Post;
 import org.sopt.dto.PostRequest;
 import org.sopt.service.PostService;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/posts")
 @RestController
 public class PostController {
     private final PostService postService;
@@ -20,27 +24,29 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping("/post")
-    public void createPost(@RequestBody final PostRequest postRequest) {
-        postService.createPost(postRequest.getTitle());
+    @PostMapping()
+    public ResponseEntity<ApiResponse<Void>> createPost(@RequestBody final PostRequest postRequest) {
+        Long createdId = postService.createPost(postRequest);
+        URI location = URI.create("/posts/" + createdId);
+        return ResponseEntity.created(location).body(ApiResponse.created());
     }
 
-    @GetMapping("/posts")
+    @GetMapping("/")
     public ResponseEntity<?> getAllPosts() {
         return ResponseEntity.ok(postService.getAllPosts());
     }
 
-    @GetMapping("/post/{id}")
+    @GetMapping("/{id}")
     public Post getPostById(@PathVariable Long id) {
         return postService.getPostById(id);
     }
 
-    @DeleteMapping("/post/{id}")
+    @DeleteMapping("/{id}")
     public void deletePostById(@PathVariable Long id) {
         postService.deletePostById(id);
     }
 
-    @PatchMapping("/post/{id}")
+    @PatchMapping("/{id}")
     public void updatePostTitle(@PathVariable Long id, @RequestBody PostRequest postRequest) {
         try {
             postService.updatePostTitle(id, postRequest.getTitle());
