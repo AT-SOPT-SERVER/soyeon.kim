014c43bfaa3361b9c55c9c159895c44751c1dbbc
kaswhy <sxshx818@naver.com>
2025-04-30 00:23:36 +0900

    feat(user): 서비스에 유저 추가 구현

---
+++ src/main/java/org/sopt/domain/user/domain/User.java
@@ -0,0 +1,32 @@
+package org.sopt.domain.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    protected User() {
+
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
