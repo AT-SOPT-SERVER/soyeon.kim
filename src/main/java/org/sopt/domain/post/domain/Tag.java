c441670fc1932fdf08b6de0a757253026d7d059d
kaswhy <sxshx818@naver.com>
2025-05-30 18:53:49 +0900

    refactor(#8/post): Tag에 getter 설정

--- src/main/java/org/sopt/domain/post/domain/Tag.java
+++ src/main/java/org/sopt/domain/post/domain/Tag.java
@@ -1,9 +1,11 @@
 package org.sopt.domain.post.domain;
 
 import java.util.Arrays;
+import lombok.Getter;
 import org.sopt.domain.post.presentation.exception.PostErrorCode;
 import org.sopt.global.error.BusinessException;
 
+@Getter
 public enum Tag {
     BACKEND("백엔드"),
     DATABASE("데이터베이스"),
@@ -13,10 +15,6 @@ public enum Tag {
 
     Tag(String name) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public static Tag fromKoreanName(String name) {
