98657af82cd39f005b41f9270e7f0c32cec14f08
kaswhy <sxshx818@naver.com>
2025-06-01 22:38:07 +0900

    chore(#8/comment): 필요없는 의존성 제거

--- src/main/java/org/sopt/comment/domain/Comment.java
+++ src/main/java/org/sopt/comment/domain/Comment.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt.global.entity.BaseEntity;
