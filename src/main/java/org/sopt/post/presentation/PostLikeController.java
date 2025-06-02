815b60e1528135fdb3529e24d95cef4f744f56f5
kaswhy <sxshx818@naver.com>
2025-06-02 23:38:37 +0900

    feat(#8/post): 게시글 좋아요 컨트롤러 및 응답 메시지 작성

--- src/main/java/org/sopt/post/presentation/PostLikeController.java
+++ src/main/java/org/sopt/post/presentation/PostLikeController.java
@@ -1,12 +1,31 @@
 package org.sopt.post.presentation;
 
+import static org.sopt.post.presentation.message.PostLikeMessage.CREATED_SUCCESS;
+
 import lombok.RequiredArgsConstructor;
-import org.sopt.post.infrastructure.repository.PostLikeRepository;
+import org.sopt.global.response.ApiResponse;
+import org.sopt.post.application.command.PostLikeCommandService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 @RestController
 public class PostLikeController {
 
-    private final PostLikeRepository postLikeRepository;
+    private final PostLikeCommandService postLikeCommandService;
+
+    @PostMapping("/{post-id}/like")
+    public ResponseEntity<ApiResponse<Void>> createPostLike(
+        @RequestHeader Long userId,
+        @PathVariable(name = "post-id") Long postId
+    ) {
+        postLikeCommandService.createPostLike(userId, postId);
+
+        return ResponseEntity.ok(ApiResponse.ok(CREATED_SUCCESS));
+    }
 }
---
+++ src/main/java/org/sopt/post/presentation/message/PostLikeMessage.java
@@ -0,0 +1,5 @@
+package org.sopt.post.presentation.message;
+
+public class PostLikeMessage {
+    public static final String CREATED_SUCCESS = "성공적으로 게시글에 좋아요를 추가했습니다.";
+}
