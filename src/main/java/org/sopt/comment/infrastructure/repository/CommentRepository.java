0ed8be5d43044278b6c25800e537a28ab48c5130
kaswhy <sxshx818@naver.com>
2025-06-02 17:12:03 +0900

    feat(#8/comment): 댓글 조회 시 삭제 여부 고려하여 메서드 작성

--- src/main/java/org/sopt/comment/infrastructure/repository/CommentRepository.java
+++ src/main/java/org/sopt/comment/infrastructure/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.sopt.comment.infrastructure.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.sopt.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findByPostId(Long postId);
+    List<Comment> findByPostIdAndDeletedFalse(Long postId);
+
+    Optional<Comment> findByIdAndDeletedFalse(Long commentId);
 }
