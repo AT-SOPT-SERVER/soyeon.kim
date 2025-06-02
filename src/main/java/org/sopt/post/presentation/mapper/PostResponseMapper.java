0ce7e28d689a0596cf543c8cf217c319dda8c7c1
kaswhy <sxshx818@naver.com>
2025-06-02 18:33:50 +0900

    feat(#8/post): 특정 게시물 조회 매퍼 로직에 댓글 목록 추가

--- src/main/java/org/sopt/post/presentation/mapper/PostResponseMapper.java
+++ src/main/java/org/sopt/post/presentation/mapper/PostResponseMapper.java
@@ -1,12 +1,15 @@
 package org.sopt.post.presentation.mapper;
 
 import org.sopt.post.application.dto.response.GetAllPostsServiceResponse;
+import org.sopt.post.application.dto.response.GetCommentServiceResponse;
 import org.sopt.post.application.dto.response.GetDetailedPostServiceResponse;
 import org.sopt.post.application.dto.response.GetSimplePostServiceResponse;
 import org.sopt.post.application.dto.response.SearchPostServiceResponse;
 import org.sopt.post.application.dto.response.SearchResultServiceResponse;
 import org.sopt.post.presentation.dto.response.AuthorResponse;
+import org.sopt.post.presentation.dto.response.CommentAuthorResponse;
 import org.sopt.post.presentation.dto.response.GetAllPostsResponse;
+import org.sopt.post.presentation.dto.response.GetCommentResponse;
 import org.sopt.post.presentation.dto.response.GetDetailedPostResponse;
 import org.sopt.post.presentation.dto.response.GetSimplePostResponse;
 import org.sopt.post.presentation.dto.response.SearchPostResponse;
@@ -37,7 +40,10 @@ public class PostResponseMapper {
             getDetailedPostServiceResponse.id(),
             AuthorResponse.from(getDetailedPostServiceResponse.authorId(), getDetailedPostServiceResponse.authorName()),
             getDetailedPostServiceResponse.title(),
-            getDetailedPostServiceResponse.content()
+            getDetailedPostServiceResponse.content(),
+            getDetailedPostServiceResponse.comments().stream()
+                .map(PostResponseMapper::toGetCommentResponse)
+                .toList()
         );
     }
 
@@ -56,6 +62,16 @@ public class PostResponseMapper {
             searchPostServiceResponse.id(),
             AuthorResponse.from(searchPostServiceResponse.authorId(), searchPostServiceResponse.authorName()),
             searchPostServiceResponse.title()
+        );
+    }
+
+    private static GetCommentResponse toGetCommentResponse(GetCommentServiceResponse serviceResponse) {
+        return new GetCommentResponse(
+            serviceResponse.id(),
+            new CommentAuthorResponse(serviceResponse.authorId(), serviceResponse.authorName()),
+            serviceResponse.content(),
+            serviceResponse.createdAt(),
+            serviceResponse.isUpdated()
         );
     }
 }
