9c66a35ae20c7fd41306b2af54d1b8b9efe2a5b2
kaswhy <sxshx818@naver.com>
2025-06-02 21:56:00 +0900

    refactor(#8/comment): joinColumn 명시

--- src/main/java/org/sopt/comment/domain/Comment.java
+++ src/main/java/org/sopt/comment/domain/Comment.java
@@ -10,6 +10,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -30,9 +31,11 @@ public class Comment extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
     private Post post;
 
     private String content;
