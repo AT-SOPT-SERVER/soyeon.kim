db6cded1c00ad690afb7df46bc073573af008148
kaswhy <sxshx818@naver.com>
2025-06-03 00:25:29 +0900

    feat(#8/post): 게시글 좋아요 누른 사람 조회하는 리포지토리 작성

--- src/main/java/org/sopt/post/infrastructure/repository/PostLikeRepository.java
+++ src/main/java/org/sopt/post/infrastructure/repository/PostLikeRepository.java
@@ -1,10 +1,16 @@
 package org.sopt.post.infrastructure.repository;
 
+import java.util.List;
 import org.sopt.post.domain.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
     boolean existsPostLikeByUserIdAndPostId(Long userId, Long postId);
+
+    int countByPostId(Long postId);
+
+    List<PostLike> findAllByPostId(Long postId);
 }
