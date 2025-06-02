36f5aa77547ee15fa446d066a145d19deb5691d9
kaswhy <sxshx818@naver.com>
2025-06-02 14:27:36 +0900

    feat(#8/comment): 댓글 목록 조회 컨트롤러 및 응답 메시지 작성

--- src/main/java/org/sopt/comment/presentation/CommentController.java
+++ src/main/java/org/sopt/comment/presentation/CommentController.java
@@ -1,17 +1,22 @@
 package org.sopt.comment.presentation;
 
 import static org.sopt.comment.presentation.mapper.CommentRequestMapper.toCreateCommentServiceRequest;
+import static org.sopt.comment.presentation.mapper.CommentResponseMapper.toGetAllCommentsResponse;
 import static org.sopt.comment.presentation.message.CommentMessage.CREATED_SUCCESS;
+import static org.sopt.comment.presentation.message.CommentMessage.RETRIEVED_ALL_SUCCESS;
 
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.sopt.comment.application.command.CommentCommandService;
 import org.sopt.comment.application.dto.request.CreateCommentServiceRequest;
+import org.sopt.comment.application.dto.response.GetAllCommentsServiceResponse;
 import org.sopt.comment.application.query.CommentQueryService;
 import org.sopt.comment.presentation.dto.request.CreateCommentRequest;
+import org.sopt.comment.presentation.dto.response.GetAllCommentsResponse;
 import org.sopt.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,5 +46,16 @@ public class CommentController {
         URI location = URI.create("/api/v1/posts/" + postId + "/comments" + commentId);
 
         return ResponseEntity.created(location).body(ApiResponse.created(CREATED_SUCCESS));
+    }
+
+    @GetMapping("/{post-id}/comments")
+    public ResponseEntity<ApiResponse<GetAllCommentsResponse>> getAllComments(
+        @RequestHeader Long userId,
+        @PathVariable(name = "post-id") Long postId
+    ) {
+        GetAllCommentsServiceResponse serviceResponse = commentQueryService.getAllComments(userId, postId);
+        GetAllCommentsResponse response = toGetAllCommentsResponse(serviceResponse);
+
+        return ResponseEntity.ok(ApiResponse.ok(RETRIEVED_ALL_SUCCESS, response));
     }
 }
--- src/main/java/org/sopt/comment/presentation/message/CommentMessage.java
+++ src/main/java/org/sopt/comment/presentation/message/CommentMessage.java
@@ -2,4 +2,5 @@ package org.sopt.comment.presentation.message;
 
 public class CommentMessage {
     public static final String CREATED_SUCCESS = "성공적으로 댓글을 등록했습니다.";
+    public static final String RETRIEVED_ALL_SUCCESS = "성공적으로 댓글 목록을 조회했습니다.";
 }
