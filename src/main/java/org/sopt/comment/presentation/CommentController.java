4636a03a933811fab783241f49df142b9fac446c
kaswhy <sxshx818@naver.com>
2025-06-02 16:36:24 +0900

    feat(#8/comment): 댓글 수정 컨트롤러 및 응답 메시지 작성

--- src/main/java/org/sopt/comment/presentation/CommentController.java
+++ src/main/java/org/sopt/comment/presentation/CommentController.java
@@ -1,22 +1,27 @@
 package org.sopt.comment.presentation;
 
 import static org.sopt.comment.presentation.mapper.CommentRequestMapper.toCreateCommentServiceRequest;
+import static org.sopt.comment.presentation.mapper.CommentRequestMapper.toUpdateCommentServiceRequest;
 import static org.sopt.comment.presentation.mapper.CommentResponseMapper.toGetAllCommentsResponse;
 import static org.sopt.comment.presentation.message.CommentMessage.CREATED_SUCCESS;
 import static org.sopt.comment.presentation.message.CommentMessage.RETRIEVED_ALL_SUCCESS;
+import static org.sopt.comment.presentation.message.CommentMessage.UPDATED_SUCCESS;
 
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.sopt.comment.application.command.CommentCommandService;
 import org.sopt.comment.application.dto.request.CreateCommentServiceRequest;
+import org.sopt.comment.application.dto.request.UpdateCommentServiceRequest;
 import org.sopt.comment.application.dto.response.GetAllCommentsServiceResponse;
 import org.sopt.comment.application.query.CommentQueryService;
 import org.sopt.comment.presentation.dto.request.CreateCommentRequest;
+import org.sopt.comment.presentation.dto.request.UpdateCommentRequest;
 import org.sopt.comment.presentation.dto.response.GetAllCommentsResponse;
 import org.sopt.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/posts")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
 public class CommentController {
@@ -32,7 +37,7 @@ public class CommentController {
     private final CommentCommandService commentCommandService;
     private final CommentQueryService commentQueryService;
 
-    @PostMapping("/{post-id}/comments")
+    @PostMapping("/posts/{post-id}/comments")
     public ResponseEntity<ApiResponse<Void>> createComment(
         @RequestHeader Long userId,
         @PathVariable(name = "post-id") Long postId,
@@ -43,12 +48,12 @@ public class CommentController {
             postId,
             createCommentRequest);
         Long commentId = commentCommandService.createComment(serviceRequest);
-        URI location = URI.create("/api/v1/posts/" + postId + "/comments" + commentId);
+        URI location = URI.create("/api/v1/posts/" + postId + "/comments/" + commentId);
 
         return ResponseEntity.created(location).body(ApiResponse.created(CREATED_SUCCESS));
     }
 
-    @GetMapping("/{post-id}/comments")
+    @GetMapping("/posts/{post-id}/comments")
     public ResponseEntity<ApiResponse<GetAllCommentsResponse>> getAllComments(
         @RequestHeader Long userId,
         @PathVariable(name = "post-id") Long postId
@@ -57,5 +62,17 @@ public class CommentController {
         GetAllCommentsResponse response = toGetAllCommentsResponse(serviceResponse);
 
         return ResponseEntity.ok(ApiResponse.ok(RETRIEVED_ALL_SUCCESS, response));
+    }
+
+    @PatchMapping("/comments/{comment-id}")
+    public ResponseEntity<ApiResponse<Void>> updateComment(
+        @RequestHeader Long userId,
+        @PathVariable(name = "comment-id") Long commentId,
+        @Valid @RequestBody UpdateCommentRequest request
+    ) {
+        UpdateCommentServiceRequest serviceRequest = toUpdateCommentServiceRequest(userId, commentId, request);
+        commentCommandService.updateComment(serviceRequest);
+
+        return ResponseEntity.ok(ApiResponse.ok(UPDATED_SUCCESS));
     }
 }
