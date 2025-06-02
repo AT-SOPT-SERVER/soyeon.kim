e1ef2eb36bc5f9257acc984112da6aa071dcf1f6
kaswhy <sxshx818@naver.com>
2025-06-02 14:26:59 +0900

    feat(#8/comment): 댓글 목록 조회 리포지토리 작성

--- src/main/java/org/sopt/comment/infrastructure/repository/CommentRepository.java
+++ src/main/java/org/sopt/comment/infrastructure/repository/CommentRepository.java
@@ -1,9 +1,12 @@
 package org.sopt.comment.infrastructure.repository;
 
+import java.util.List;
 import org.sopt.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPostId(Long postId);
 }
