a0484822107455c1f7d74ff72fd374a50798ae8d
kaswhy <sxshx818@naver.com>
2025-06-14 23:12:41 +0900

    feat(#8/post): 특정 게시물 좋아요 수 반환 컨트롤러/성공 메시지 작성

--- src/main/java/org/sopt/post/presentation/PostLikeController.java
+++ src/main/java/org/sopt/post/presentation/PostLikeController.java
@@ -1,14 +1,17 @@
 package org.sopt.post.presentation;
 
+import static org.sopt.post.presentation.message.PostLikeMessage.COUNT_RETRIEVED_SUCCESS;
 import static org.sopt.post.presentation.message.PostLikeMessage.CREATED_SUCCESS;
-import static org.sopt.post.presentation.message.PostLikeMessage.RETRIEVED_SUCCESS;
+import static org.sopt.post.presentation.message.PostLikeMessage.USERS_RETRIEVED_SUCCESS;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.global.response.ApiResponse;
 import org.sopt.post.application.command.PostLikeCommandService;
 import org.sopt.post.application.dto.response.GetUsersLikedPostServiceResponse;
+import org.sopt.post.application.dto.response.LikeCountServiceResponse;
 import org.sopt.post.application.query.PostLikeQueryService;
 import org.sopt.post.presentation.dto.response.GetUsersLikedResponse;
+import org.sopt.post.presentation.dto.response.LikeCountResponse;
 import org.sopt.post.presentation.mapper.PostLikeResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +46,19 @@ public class PostLikeController {
         GetUsersLikedPostServiceResponse serviceResponse = postLikeQueryService.getUsersLikedPost(postId);
         GetUsersLikedResponse response = PostLikeResponseMapper.toGetUsersLikedResponse(serviceResponse);
 
-        return ResponseEntity.ok(ApiResponse.ok(RETRIEVED_SUCCESS, response));
+        return ResponseEntity.ok(ApiResponse.ok(USERS_RETRIEVED_SUCCESS, response));
     }
+
+    @GetMapping("/{post-id}/likes/count")
+    public ResponseEntity<ApiResponse<LikeCountResponse>> getLikeCountByPostId(
+        @PathVariable(name = "post-id") Long postId
+    ) {
+        LikeCountServiceResponse serviceResponse = postLikeQueryService.getLikeCount(postId);
+        LikeCountResponse response = PostLikeResponseMapper.toLikeCountResponse(serviceResponse);
+
+        return ResponseEntity.ok(ApiResponse.ok(COUNT_RETRIEVED_SUCCESS, response));
+    }
+
+    // TODO 게시글 좋아요 취소
+
 }
--- src/main/java/org/sopt/post/presentation/message/PostLikeMessage.java
+++ src/main/java/org/sopt/post/presentation/message/PostLikeMessage.java
@@ -2,5 +2,7 @@ package org.sopt.post.presentation.message;
 
 public class PostLikeMessage {
     public static final String CREATED_SUCCESS = "성공적으로 게시글에 좋아요를 추가했습니다.";
-    public static final String RETRIEVED_SUCCESS = "성공적으로 게시글에 좋아요를 누른 유저 목록을 조회했습니다.";
+    public static final String USERS_RETRIEVED_SUCCESS = "성공적으로 게시글에 좋아요를 누른 유저 목록을 조회했습니다.";
+    public static final String COUNT_RETRIEVED_SUCCESS = "성공적으로 게시글에 눌린 좋아요 수를 조회했습니다.";
+
 }
