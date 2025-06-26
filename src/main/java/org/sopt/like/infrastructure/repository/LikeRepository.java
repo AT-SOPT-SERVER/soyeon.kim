e5a3102ba77841f50fcc65b33122ff60f31796ee
kaswhy <sxshx818@naver.com>
2025-06-26 15:19:31 +0900

    refactor(#8/like): 사용하지 않는 like 패키지 삭제

--- src/main/java/org/sopt/like/infrastructure/repository/LikeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.like.infrastructure.repository;
-
-import org.sopt.like.domain.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}
