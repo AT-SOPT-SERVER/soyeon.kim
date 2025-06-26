33f03cb0a250cf4fb772f42dc70117ac0431cbd8
kaswhy <sxshx818@naver.com>
2025-06-26 14:02:36 +0900

    feat(#8/comment): 댓글 좋아요 수 반환 컨트롤러, 성공 메시지 구현

--- src/main/java/org/sopt/comment/presentation/CommentLikeController.java
+++ src/main/java/org/sopt/comment/presentation/CommentLikeController.java
@@ -1,13 +1,19 @@
 package org.sopt.comment.presentation;
 
 import static org.sopt.comment.presentation.message.CommentLikeMessage.CREATED_SUCCESS;
+import static org.sopt.comment.presentation.message.CommentLikeMessage.RETRIEVED_SUCCESS;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.comment.application.command.CommentLikeCommandService;
+import org.sopt.comment.application.dto.response.GetCommentLikesCountServiceResponse;
 import org.sopt.comment.application.query.CommentLikeQueryService;
+import org.sopt.comment.presentation.dto.response.GetCommentLikesCountResponse;
+import org.sopt.comment.presentation.mapper.CommentLikeResponseMapper;
 import org.sopt.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +26,8 @@ public class CommentLikeController {
     private final CommentLikeCommandService commentLikeCommandService;
     private final CommentLikeQueryService commentLikeQueryService;
 
-    @RequestMapping("/{comment-id}/likes")
-    public ResponseEntity<ApiResponse<Void>> postCommentLike(
+    @PostMapping("/{comment-id}/likes")
+    public ResponseEntity<ApiResponse<Void>> postCommentLikes(
         @PathVariable(name = "comment-id") Long commentId,
         @RequestHeader Long userId
     ) {
@@ -30,7 +36,16 @@ public class CommentLikeController {
         return ResponseEntity.ok(ApiResponse.created(CREATED_SUCCESS));
     }
 
-    // TODO 댓글 좋아요 수 반환
+    @GetMapping("/{comment-id}/likes")
+    public ResponseEntity<ApiResponse<GetCommentLikesCountResponse>> getCommentLikesCount(
+        @PathVariable(name = "comment-id") Long commentId
+    ) {
+        GetCommentLikesCountServiceResponse serviceResponse = commentLikeQueryService.getCommentLikesCount(commentId);
+        GetCommentLikesCountResponse response =
+            CommentLikeResponseMapper.toGetCommentLikesCountResponse(serviceResponse);
+
+        return ResponseEntity.ok(ApiResponse.ok(RETRIEVED_SUCCESS, response));
+    }
 
     // TODO 댓글 좋아요 취소
 
--- src/main/java/org/sopt/comment/presentation/message/CommentLikeMessage.java
+++ src/main/java/org/sopt/comment/presentation/message/CommentLikeMessage.java
@@ -2,4 +2,5 @@ package org.sopt.comment.presentation.message;
 
 public class CommentLikeMessage {
     public static String CREATED_SUCCESS = "성공적으로 댓글에 좋아요를 등록했습니다.";
+    public static String RETRIEVED_SUCCESS = "성공적으로 댓글 좋아요 수를 반환했습니다.";
 }
