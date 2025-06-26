09cc67bd362242d47b18b18dda671a6c741327fb
kaswhy <sxshx818@naver.com>
2025-06-26 13:17:59 +0900

    feat(#8/comment): 댓글 좋아요 리포지토리 구현

--- src/main/java/org/sopt/comment/infrastructure/repository/CommentLikeRepository.java
+++ src/main/java/org/sopt/comment/infrastructure/repository/CommentLikeRepository.java
@@ -1,7 +1,11 @@
 package org.sopt.comment.infrastructure.repository;
 
+import org.sopt.comment.domain.Comment;
 import org.sopt.comment.domain.CommentLike;
+import org.sopt.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    boolean existsByUserAndComment(User user, Comment comment);
 }
