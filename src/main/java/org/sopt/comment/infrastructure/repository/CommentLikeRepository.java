8d10ae151650dabc4feef74cee256e678c22bead
kaswhy <sxshx818@naver.com>
2025-06-26 14:02:07 +0900

    feat(#8/comment): 댓글 좋아요 수 반환 리포지토리 구현

--- src/main/java/org/sopt/comment/infrastructure/repository/CommentLikeRepository.java
+++ src/main/java/org/sopt/comment/infrastructure/repository/CommentLikeRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 
     boolean existsByUserAndComment(User user, Comment comment);
+
+    int countByComment(Comment comment);
 }
