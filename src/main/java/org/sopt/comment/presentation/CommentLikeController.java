4a6ee0a6050858cdffe52bb61b16453417aa6060
kaswhy <sxshx818@naver.com>
2025-06-26 13:18:28 +0900

    feat(#8/comment): 댓글 좋아요 컨트롤러/성공 메시지 구현

--- src/main/java/org/sopt/comment/presentation/CommentLikeController.java
+++ src/main/java/org/sopt/comment/presentation/CommentLikeController.java
@@ -1,8 +1,14 @@
 package org.sopt.comment.presentation;
 
+import static org.sopt.comment.presentation.message.CommentLikeMessage.CREATED_SUCCESS;
+
 import lombok.RequiredArgsConstructor;
 import org.sopt.comment.application.command.CommentLikeCommandService;
 import org.sopt.comment.application.query.CommentLikeQueryService;
+import org.sopt.global.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +19,19 @@ public class CommentLikeController {
 
     private final CommentLikeCommandService commentLikeCommandService;
     private final CommentLikeQueryService commentLikeQueryService;
+
+    @RequestMapping("/{comment-id}/likes")
+    public ResponseEntity<ApiResponse<Void>> postCommentLike(
+        @PathVariable(name = "comment-id") Long commentId,
+        @RequestHeader Long userId
+    ) {
+        commentLikeCommandService.likeComment(userId, commentId);
+
+        return ResponseEntity.ok(ApiResponse.created(CREATED_SUCCESS));
+    }
+
+    // TODO 댓글 좋아요 수 반환
+
+    // TODO 댓글 좋아요 취소
 
 }
---
+++ src/main/java/org/sopt/comment/presentation/message/CommentLikeMessage.java
@@ -0,0 +1,5 @@
+package org.sopt.comment.presentation.message;
+
+public class CommentLikeMessage {
+    public static String CREATED_SUCCESS = "성공적으로 댓글에 좋아요를 등록했습니다.";
+}
